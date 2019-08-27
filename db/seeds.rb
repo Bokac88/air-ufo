368e31f554fa786a410a2d5c72c32d68cd3d8dd1
Francisco Gaspar <email.francisco.g@gmail.com>
2019-08-27 14:36:50 +0100

    photo randomize

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 puts 'Creating 20 fake users...'
-20.times do
+20.times do |n|
   user = User.new(
     email: Faker::Internet.email,
-    password: Faker::Internet.password(min_length: 10, max_length: 20)
+    password: Faker::Internet.password(min_length: 10, max_length: 20),
+    photo: "https://randomuser.me/api/portraits/med/men/#{n}.jpg"
 )
   user.save!
 end
