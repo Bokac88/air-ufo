a48bc32a2bc15566a08c81897473ab34796d12a0
Boris Durisic <bokac279@gmail.com>
2019-08-27 12:58:36 +0100

    destroy

--- app/controllers/ufos_controller.rb
+++ app/controllers/ufos_controller.rb
@@ -1,4 +1,5 @@
 class UfosController < ApplicationController
+  before_action :get_ufo, only: [ :show, :destroy ]
   skip_before_action :authenticate_user!, only: :index
 
   def index
@@ -21,13 +22,24 @@ class UfosController < ApplicationController
   end
 
   def show
-    @ufo = Ufo.find(params[:id])
     authorize @ufo
+  end
+
+  def destroy
+    # byebug
+    @ufo.destroy
+    authorize @ufo
+
+    redirect_to root_path
   end
 
   private
 
   def ufo_params
     params.require(:ufo).permit(:brand, :speed, :age, :price, :description)
+  end
+
+  def get_ufo
+    @ufo = Ufo.find(params[:id])
   end
 end
--- app/policies/ufo_policy.rb
+++ app/policies/ufo_policy.rb
@@ -12,12 +12,13 @@ class UfoPolicy < ApplicationPolicy
   def show?
     true
   end
-
+  
   # def update?
   #   record.user == user
   # end
 
-  # def destroy?
-  #   record.user == user
-  # end
+  def destroy?
+    # record.owner == user
+    true
+  end
 end
