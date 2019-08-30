201e89adc1ac8b52ece2cb508e30901d934f2c72
Boris Durisic <bokac279@gmail.com>
2019-08-30 15:25:32 +0100

    done

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,5 @@
 class Rental < ApplicationRecord
   belongs_to :customer, class_name: "User", foreign_key: :customer_id
-  belongs_to :ufo, dependent: :destroy
+  belongs_to :ufo
   has_one :review
 end
